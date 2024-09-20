2506565163866072bee6d79e5fd6096b5f6c82c2
diksha.khandelwal <diksha.khandelwal@daffodilsw.com>
2024-09-20 12:53:29 +0530

    added firebase code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './_components/login/login.component';
+import { SignUpComponent } from './_components/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'signup', pathMatch: 'full'},
+  {path:'login', component: LoginComponent},
+  {path: 'signup', component:SignUpComponent},
+  {path: '**', redirectTo: 'login', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environment/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { SignUpComponent } from './_components/sign-up/sign-up.component';
+import { LoginComponent } from './_components/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';  // Import ReactiveFormsModule
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp(environment.firebaseConfig), // Initialize Firebase
+    AngularFireAuthModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
